595c49accc57178402a8955c67855755bc701a12
Tyler <nuvallo.inc@gmail.com>
2020-01-23 18:00:48 -0500

    All buttons are working correctly

--- baseball-app/src/components/Dashboard.js
+++ baseball-app/src/components/Dashboard.js
@@ -22,16 +22,45 @@ function Dashboard(props) {
         >
           Hit for away
         </button>
-        <button>Strike</button>
+        <button
+          onClick={() => {
+            let values = addStrike(props.strikes, props.outs);
+            props.setStrikes(values[0]);
+            props.setOuts(values[1]);
+          }}
+        >
+          Strike
+        </button>
         <button onClick={() => props.setBalls(addBall(props.balls))}>
           Ball
         </button>
-        <button>Out</button>
-        <button>Foul</button>
+        <button
+          onClick={() => {
+            let values = addStrike(3, props.outs);
+            props.setBalls(0);
+            props.setStrikes(0);
+            props.setOuts(values[1]);
+          }}
+        >
+          Out
+        </button>
+        <button onClick={() => props.setStrikes(addFoul(props.strikes))}>
+          Foul
+        </button>
       </div>
     </div>
   );
 }
+
+const addStrike = (prevStrike, prevOut) => {
+  let newStrike = prevStrike + 1;
+  let newOut = prevOut;
+  if (newStrike > 2) {
+    newStrike = 0;
+    newOut = addOut(prevOut);
+  }
+  return [newStrike, newOut];
+};
 
 const addBall = count => {
   let newCount = count + 1;
@@ -40,5 +69,25 @@ const addBall = count => {
     newCount = 0;
   }
   return newCount;
+};
+
+const addOut = prevOut => {
+  let newOut = prevOut + 1;
+
+  if (newOut > 2) {
+    newOut = 0;
+  }
+
+  return newOut;
+};
+
+const addFoul = prevStrike => {
+  let newStrike = prevStrike + 1;
+
+  if (newStrike > 2) {
+    newStrike = 2;
+  }
+
+  return newStrike;
 };
 export default Dashboard;
